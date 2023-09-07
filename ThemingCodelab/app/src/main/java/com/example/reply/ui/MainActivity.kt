d716b6488ea41ea4dddc16becbb3794650f26f57
Lexie Ludeman <lexie.ludeman@gmail.com>
2023-09-07 15:32:25 -0600

    Completed tutorial

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -22,48 +22,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.reply.data.LocalEmailsDataProvider
-
+import com.example.reply.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: ReplyHomeViewModel by viewModels()
+  private val viewModel: ReplyHomeViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        setContent {
-            val uiState by viewModel.uiState.collectAsState()
-            ReplyApp(
-                replyHomeUIState = uiState,
-                closeDetailScreen = {
-                    viewModel.closeDetailScreen()
-                },
-                navigateToDetail = { emailId ->
-                    viewModel.setSelectedEmail(emailId)
-                }
-            )
+    setContent {
+      val uiState by viewModel.uiState.collectAsState()
+      AppTheme {
+        Surface(tonalElevation = 5.dp) {
+          ReplyApp(
+              replyHomeUIState = uiState,
+              closeDetailScreen = { viewModel.closeDetailScreen() },
+              navigateToDetail = { emailId -> viewModel.setSelectedEmail(emailId) })
         }
+      }
     }
+  }
 }
 
-@Preview(
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
-@Preview(
-    uiMode = UI_MODE_NIGHT_NO,
-    name = "DefaultPreviewLight"
-)
+@Preview(uiMode = UI_MODE_NIGHT_YES, name = "Dark")
+@Preview(uiMode = UI_MODE_NIGHT_NO, name = "Light")
 @Composable
-fun ReplyAppPreviewLight() {
-    ReplyApp(
-        replyHomeUIState = ReplyHomeUIState(
-            emails = LocalEmailsDataProvider.allEmails
-        )
-    )
+fun ReplyAppPreview() {
+  AppTheme {
+    ReplyApp(replyHomeUIState = ReplyHomeUIState(emails = LocalEmailsDataProvider.allEmails))
+  }
 }
--- ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
@@ -26,9 +26,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.StarBorder
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -48,57 +49,52 @@ fun ReplyEmailListItem(
     modifier: Modifier = Modifier,
     navigateToDetail: (Long) -> Unit
 ) {
-    Card(
-        modifier =  modifier
-            .padding(horizontal = 16.dp, vertical = 4.dp)
-            .semantics { selected = isSelected }
-            .clickable { navigateToDetail(email.id) },
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp)
-        ) {
-            Row(modifier = Modifier.fillMaxWidth()) {
-                ReplyProfileImage(
-                    drawableResource = email.sender.avatar,
-                    description = email.sender.fullName,
-                )
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(horizontal = 12.dp, vertical = 4.dp),
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = email.sender.firstName,
-                    )
-                    Text(
-                        text = email.createdAt,
-                    )
-                }
-                IconButton(
-                    onClick = { /*Click Implementation*/ },
-                    modifier = Modifier
-                        .clip(CircleShape)
-
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.StarBorder,
-                        contentDescription = stringResource(id = R.string.description_favorite),
-                    )
-                }
-            }
-
-            Text(
-                text = email.subject,
-                modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
+  Card(
+      modifier =
+          modifier
+              .padding(horizontal = 16.dp, vertical = 4.dp)
+              .semantics { selected = isSelected }
+              .clickable { navigateToDetail(email.id) },
+      colors =
+          CardDefaults.cardColors(
+              containerColor =
+                  if (email.isImportant) MaterialTheme.colorScheme.secondaryContainer
+                  else MaterialTheme.colorScheme.surfaceVariant)) {
+        Column(modifier = Modifier.fillMaxWidth().padding(20.dp)) {
+          Row(modifier = Modifier.fillMaxWidth()) {
+            ReplyProfileImage(
+                drawableResource = email.sender.avatar,
+                description = email.sender.fullName,
             )
-            Text(
-                text = email.body,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
+            Column(
+                modifier = Modifier.weight(1f).padding(horizontal = 12.dp, vertical = 4.dp),
+                verticalArrangement = Arrangement.Center) {
+                  Text(text = email.sender.firstName, style = MaterialTheme.typography.labelMedium)
+                  Text(
+                      text = email.createdAt,
+                      style = MaterialTheme.typography.labelMedium,
+                      color = MaterialTheme.colorScheme.onSurfaceVariant)
+                }
+            IconButton(
+                onClick = { /*Click Implementation*/}, modifier = Modifier.clip(CircleShape)) {
+                  Icon(
+                      imageVector = Icons.Default.StarBorder,
+                      contentDescription = stringResource(id = R.string.description_favorite),
+                  )
+                }
+          }
+
+          Text(
+              text = email.subject,
+              style = MaterialTheme.typography.titleLarge,
+              modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
+          )
+          Text(
+              text = email.body,
+              maxLines = 2,
+              style = MaterialTheme.typography.bodyLarge,
+              color = MaterialTheme.colorScheme.onSurfaceVariant,
+              overflow = TextOverflow.Ellipsis)
         }
-    }
+      }
 }
